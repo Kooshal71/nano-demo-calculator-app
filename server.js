2fea7546f10c1fbc6dc726bf8af1a5e56a3c863b
Kooshal71 <kushalganesh2@gmail.com>
2023-08-21 16:34:28 +0530

    Completed add and subtract

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const baseUrl = '/calculator'
+const baseUrl = "/calculator";
 
 app.use(express.json());
 
 const baseRouter = express.Router();
 
-baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+baseRouter.get("/greeting", (req, res) => {
+  return res.send("Hello world!");
 });
 
-baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+baseRouter.post("/add", (req, res) => {
+  let ans = req.body.first + req.body.second;
+  res.status(200).json({ result: ans });
 });
 
-
-baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+baseRouter.post("/subtract", (req, res) => {
+  let ans = req.body.first - req.body.second;
+  res.status(200).json({ result: ans });
 });
 
 app.use(baseUrl, baseRouter);
 app.listen(PORT, () => {
-    console.log("Server running at PORT", PORT);
+  console.log("Server running at PORT", PORT);
 });
